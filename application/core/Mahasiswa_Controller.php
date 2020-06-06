d341f1061f729df50e1498ad8d7c9242f9e1c708
yaufaniadam <yaufani@gmail.com>
2020-06-06 09:33:29 +0700

    mumet

---
+++ application/core/Mahasiswa_Controller.php
@@ -0,0 +1,15 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+class Mahasiswa_Controller extends MY_Controller
+{
+    function __construct()
+    {
+        parent::__construct();
+        
+        if( ($this->session->userdata('role') != 3 ) )
+        {
+            $data['view'] = 'layout/restricted';
+		    $this->load->view('layout/layout', $data);   
+            
+        } 
+    }
+}
